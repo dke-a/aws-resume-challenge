e9fb0792e62eb82ce795478a021dcef67628c639
dke-a <angeles.don@yahoo.com>
2023-11-15 14:57:26 -0500

    added backend config for S3 remote state bucket.

--- provider.tf
+++ provider.tf
@@ -4,6 +4,14 @@ terraform {
       source = "hashicorp/aws"
     }
   }
+
+  backend "s3" {
+    bucket         = "resume-tf-state-bucket"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "resume_tf_state_lock"
+    encrypt        = true
+  }
 }
 
 # Configure the AWS Provider
