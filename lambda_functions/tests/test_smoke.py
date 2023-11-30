b29092fd8311487d0556e7cac398a91e28bb0785
dke-a <angeles.don@yahoo.com>
2023-11-29 21:36:59 -0500

    modify smoke test accommodate POST request and proper update of DDB.

--- lambda_functions/tests/test_smoke.py
+++ lambda_functions/tests/test_smoke.py
@@ -1,28 +1,41 @@
 import requests
 import json
 
+import boto3
+
 # The URL of the deployed API
 API_URL = "https://9drbe81ae7.execute-api.us-east-1.amazonaws.com/countVisit"
 
-def test_api_returns_updated_value():
-    # Send a request to the API
-    response = requests.get(API_URL)
-    assert response.status_code == 200
-    # Parse the response body to get the visitor count
-    body = json.loads(response.text)
-    initial_count = int(response.text)#body['visitorCount']
+def get_visit_count_from_dynamodb():
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')  # Replace 'us-east-1' with your region
+    table = dynamodb.Table('visitCounter')
+    response = table.get_item(Key={'id': 'counter'})
+    return int(response['Item']['visit_count'])
 
-    # Send another request to increment the visitor count
-    response = requests.get(API_URL)
+def test_api_returns_updated_value():
+    # Send a POST request to the API to increment the visitor count
+    response = requests.post(API_URL)
+    assert response.status_code == 200
+    # Parse the response body to get the visitor count after the first increment
+    body = json.loads(response.text)
+    initial_count = int(body)  # Assuming the response contains the count directly
+
+    # Send another POST request to increment the visitor count again
+    response = requests.post(API_URL)
     assert response.status_code == 200
     body = json.loads(response.text)
-    updated_count = int(response.text)#body['visitorCount']
+    updated_count = int(body)
 
     # Check if the visitor count has incremented
     assert updated_count == initial_count + 1
 
-    # Optionally, you can add another check to query the database directly (if accessible)
-    # to verify that the count is also incremented there.
+    # Get the current count from DynamoDB directly
+    dynamodb_count = get_visit_count_from_dynamodb()
+
+    # Check if the DynamoDB count matches the updated count
+    assert dynamodb_count == updated_count
+
+
 
 def test_api_response_to_get_request():
     # Send a GET request which is not the expected method for the API
